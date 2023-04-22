a3e88f42f16c46c604152c35d227014350f6a44c
222crafter <106040054+222crafter@users.noreply.github.com>
2023-04-21 21:45:13 -0400

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -1,17 +1,54 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $message = test_input($_POST["message"]);
+    
+    $to = "ymn.caglar@gmail.com";
+    $subject = "[SUPER IMPORTANT] NEW MESSAGE ABOUT FIRE DUDE APPLICATION";
+    $message = "This is an automated message issued by " . $name . "\n\n" .
+               "Name: " . $name . "\n\n" .
+               "Email: " . $email . "\n\n" .
+               "Message: " . $message;
+    $headers = "From: " . $email;
 
-  $to = "your-email@example.com"; // Replace with your own email address
-  $subject = "New message from $name";
-  $body = "Name: $name\nEmail: $email\nMessage: $message";
+    mail($to, $subject, $message, $headers);
 
-  if (mail($to, $subject, $body)) {
-    echo "Message sent successfully!";
-  } else {
-    echo "Failed to send message.";
-  }
 }
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert form data into database
+$sql = "INSERT INTO contacts (name, email, message)
+        VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+echo "Thank you for your message, $name! We'll get back to you soon.";
+
 ?>
